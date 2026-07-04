53321fbc5b4610d4188e83b2b43a28cd28a3abb6
Maria Eduarda Moura Crusco <mariaeduardam.crusco@hotmail.com>
2026-07-03 22:24:56 -0300

    Finalizado

--- T6/simlang/casos_de_teste/sim_valido_completo.py
+++ T6/simlang/casos_de_teste/sim_valido_completo.py
@@ -1,23 +1,35 @@
 import sims4.commands
 
-@sims4.commands.Command('aplicar_simlang', command_type=sims4.commands.CommandType.Live)
+@sims4.commands.Command('simlang_ping', command_type=sims4.commands.CommandType.Live, console_type=sims4.commands.CommandType.Cheat)
+def simlang_ping(_connection=None):
+    output = sims4.commands.CheatOutput(_connection)
+    output('SimLang carregado com sucesso.')
+
+@sims4.commands.Command('aplicar_simlang', 'simlang', command_type=sims4.commands.CommandType.Live, console_type=sims4.commands.CommandType.Cheat)
 def aplicar_simlang(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
+    def executar_comando(comando):
+        sims4.commands.client_cheat('|' + comando, _connection)
+
     output('Aplicando mods gerados pela SimLang...')
-    sims4.commands.client_cheat('motherlode', _connection)
+    try:
+        executar_comando('testingcheats on')
+        executar_comando('motherlode')
 
-    # Configurando o Sim: Maria Eduarda
-    sims4.commands.client_cheat('traits.equip_trait trait_Genius', _connection)
-    sims4.commands.client_cheat('traits.equip_trait trait_Cheerful', _connection)
-    sims4.commands.client_cheat('traits.equip_trait trait_Ambitious', _connection)
-    sims4.commands.client_cheat('stats.set_skill_level Major_Programming 8', _connection)
-    sims4.commands.client_cheat('stats.set_skill_level Major_Logic 7', _connection)
+        # Configurando o Sim: Maria Eduarda
+        executar_comando('traits.equip_trait Genius')
+        executar_comando('traits.equip_trait Cheerful')
+        executar_comando('traits.equip_trait Ambitious')
+        executar_comando('stats.set_skill_level Major_Programming 8')
+        executar_comando('stats.set_skill_level Major_Logic 7')
 
-    # Configurando o Sim: Guilherme
-    sims4.commands.client_cheat('traits.equip_trait trait_SelfAssured', _connection)
-    sims4.commands.client_cheat('traits.equip_trait trait_Focused', _connection)
-    sims4.commands.client_cheat('stats.set_skill_level Major_ResearchDebate 10', _connection)
-    sims4.commands.client_cheat('stats.set_skill_level Major_Charisma 5', _connection)
-    sims4.commands.client_cheat('stats.set_skill_level Major_Logic 9', _connection)
+        # Configurando o Sim: Guilherme
+        executar_comando('traits.equip_trait SelfAssured')
+        executar_comando('traits.equip_trait Focused')
+        executar_comando('stats.set_skill_level Major_ResearchDebate 10')
+        executar_comando('stats.set_skill_level Major_Charisma 5')
+        executar_comando('stats.set_skill_level Major_Logic 9')
 
-    output('Todos os atributos foram aplicados com sucesso!')
+        output('Todos os atributos foram aplicados com sucesso!')
+    except Exception as exc:
+        output('Erro ao aplicar SimLang: {}'.format(exc))
--- T6/simlang/src/main/java/br/ufscar/dc/compiladores/linguagem/la/SimLangGeradorPython.java
+++ T6/simlang/src/main/java/br/ufscar/dc/compiladores/linguagem/la/SimLangGeradorPython.java
@@ -15,12 +15,12 @@ public class SimLangGeradorPython extends SimLangBaseVisitor<Void> {
     public SimLangGeradorPython(TabelaSimbolos tabela) {
         this.tabela = tabela;
 
-        mapTracos.put("Genio", "trait_Genius");
-        mapTracos.put("Alegre", "trait_Cheerful");
-        mapTracos.put("Ambiciosa", "trait_Ambitious");
-        mapTracos.put("Ambicioso", "trait_Ambitious");
-        mapTracos.put("Seguro", "trait_SelfAssured");
-        mapTracos.put("Focado", "trait_Focused");
+        mapTracos.put("Genio", "Genius");
+        mapTracos.put("Alegre", "Cheerful");
+        mapTracos.put("Ambiciosa", "Ambitious");
+        mapTracos.put("Ambicioso", "Ambitious");
+        mapTracos.put("Seguro", "SelfAssured");
+        mapTracos.put("Focado", "Focused");
 
         mapHabilidades.put("Programacao", "Major_Programming");
         mapHabilidades.put("Logica", "Major_Logic");
@@ -31,17 +31,25 @@ public class SimLangGeradorPython extends SimLangBaseVisitor<Void> {
     @Override
     public Void visitPrograma(SimLangParser.ProgramaContext ctx) {
         saida.append("import sims4.commands\n\n");
-        saida.append("@sims4.commands.Command('aplicar_simlang', command_type=sims4.commands.CommandType.Live)\n");
+        saida.append("@sims4.commands.Command('simlang_ping', command_type=sims4.commands.CommandType.Live, console_type=sims4.commands.CommandType.Cheat)\n");
+        saida.append("def simlang_ping(_connection=None):\n");
+        saida.append("    output = sims4.commands.CheatOutput(_connection)\n");
+        saida.append("    output('SimLang carregado com sucesso.')\n\n");
+        saida.append("@sims4.commands.Command('aplicar_simlang', 'simlang', command_type=sims4.commands.CommandType.Live, console_type=sims4.commands.CommandType.Cheat)\n");
         saida.append("def aplicar_simlang(_connection=None):\n");
         saida.append("    output = sims4.commands.CheatOutput(_connection)\n");
+        saida.append("    def executar_comando(comando):\n");
+        saida.append("        sims4.commands.client_cheat('|' + comando, _connection)\n\n");
         saida.append("    output('Aplicando mods gerados pela SimLang...')\n");
-        
-        // A PROVA REAL: Injete 50.000 simoleons! Se isso funcionar, o seu compilador venceu.
-        saida.append("    sims4.commands.client_cheat('motherlode', _connection)\n\n");
+        saida.append("    try:\n");
+        saida.append("        executar_comando('testingcheats on')\n");
+        saida.append("        executar_comando('motherlode')\n\n");
 
         super.visitPrograma(ctx);
 
-        saida.append("    output('Todos os atributos foram aplicados com sucesso!')\n");
+        saida.append("        output('Todos os atributos foram aplicados com sucesso!')\n");
+        saida.append("    except Exception as exc:\n");
+        saida.append("        output('Erro ao aplicar SimLang: {}'.format(exc))\n");
         return null;
     }
 
@@ -50,18 +58,18 @@ public class SimLangGeradorPython extends SimLangBaseVisitor<Void> {
         simAtualNome = ctx.NOME_SIM().getText().replace("\"", "");
         TabelaSimbolos.RegistroSim sim = tabela.getSim(simAtualNome);
 
-        saida.append("    # Configurando o Sim: ").append(simAtualNome).append("\n");
+        saida.append("        # Configurando o Sim: ").append(simAtualNome).append("\n");
 
         for (String traco : sim.tracos) {
-            String cheatTraco = mapTracos.getOrDefault(traco, "trait_" + traco);
-            saida.append("    sims4.commands.client_cheat('traits.equip_trait ")
-                 .append(cheatTraco).append("', _connection)\n");
+            String cheatTraco = mapTracos.getOrDefault(traco, traco);
+            saida.append("        executar_comando('traits.equip_trait ")
+                 .append(cheatTraco).append("')\n");
         }
 
         for (Map.Entry<String, Integer> hab : sim.habilidades.entrySet()) {
             String cheatHab = mapHabilidades.getOrDefault(hab.getKey(), "Major_" + hab.getKey());
-            saida.append("    sims4.commands.client_cheat('stats.set_skill_level ")
-                 .append(cheatHab).append(" ").append(hab.getValue()).append("', _connection)\n");
+            saida.append("        executar_comando('stats.set_skill_level ")
+                 .append(cheatHab).append(" ").append(hab.getValue()).append("')\n");
         }
         saida.append("\n");
 
